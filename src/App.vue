6ccc3151b89026ed17c5e298706dfb2f376d4405
KSentak <k.sentak@outlook.com>
2021-01-05 21:19:45 -0500

    finish styling

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ main {
   margin-bottom: 30px;
 }
 
-.search-box .search-bar {
+.search-bar {
   display: block;
   width: 100%;
   padding: 15px;
@@ -70,8 +70,49 @@ main {
   transition: 0.4s;
 }
 
-.search-box .search-bar:focus {
+.search-bar:focus {
   box-shadow: 16px rgba(0, 0, 0, 0, 25);
   background-color: rgba(255, 255, 255, 0.75);
+}
+
+.location {
+  color: white;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.date {
+  color: white;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.temperature {
+  display: inline-block;
+  padding: 10px 25px;
+  color: white;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-status {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0, 25);
 }
 </style>
